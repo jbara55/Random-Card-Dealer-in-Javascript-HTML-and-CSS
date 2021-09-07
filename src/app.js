175bb52c07d53c5943512201f7c5b29878858770
jbara55 <barahona.jorge@gmail.com>
2021-09-07 03:27:27 +0000

    fin

--- src/app.js
+++ src/app.js
@@ -2,19 +2,17 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
   let pinta = ["♦", "♥", "♣", "♠"];
   let card = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "k"];
 
-  let num_card = card[Math.floor(Math.random() * card.length)];
-  let num_pinta = pinta[Math.floor(Math.random() * pinta.length)];
-  let top = document.querySelector(".top");
-  top.innerHTML = num_pinta;
-  let medio = document.querySelector(".medio");
-  medio.innerHTML = num_card;
-  let bottom = document.querySelector(".bottom");
-  bottom.innerHTML = num_pinta;
+  let random_pinta = pinta[Math.floor(Math.random() * pinta.length)];
+  let random_numero = card[Math.floor(Math.random() * card.length)];
+
+  let result = [random_pinta];
+  let result2 = [random_numero];
+
+  document.querySelector(".top").innerHTML = result;
+  document.querySelector(".numero").innerHTML = result2;
+  document.querySelector(".bottom").innerHTML = result;
 };
