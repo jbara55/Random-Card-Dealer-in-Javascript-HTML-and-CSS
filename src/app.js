f4c58f98c82361effb05384d95a99ff381a65b5d
jbara55 <barahona.jorge@gmail.com>
2021-09-07 03:14:42 +0000

    final

--- src/app.js
+++ src/app.js
@@ -6,6 +6,15 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pinta = ["♦", "♥", "♣", "♠"];
+  let card = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "k"];
+
+  let num_card = card[Math.floor(Math.random() * card.length)];
+  let num_pinta = pinta[Math.floor(Math.random() * pinta.length)];
+  let top = document.querySelector(".top");
+  top.innerHTML = num_pinta;
+  let medio = document.querySelector(".medio");
+  medio.innerHTML = num_card;
+  let bottom = document.querySelector(".bottom");
+  bottom.innerHTML = num_pinta;
 };
